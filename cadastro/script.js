c6ffc523b817f8fb4725d5769ba475ff374050bb
mgcvale <miguelcvalealt@gmail.com>
2024-11-13 11:29:00 -0300

    mdtymkdty

--- cadastro/script.js
+++ cadastro/script.js
@@ -95,3 +95,26 @@ const telefoneInput = document.getElementById('telefone');
       }
     });
 
+
+
+
+
+
+
+
+    
+    const toggleLink = document.getElementById('imgConta');
+    const popup = document.getElementById('popUp');
+
+    // Show the div when the link is clicked
+    toggleLink.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default link behavior
+        popup.classList.toggle('hidden'); // Toggle visibility of the div
+    });
+
+    // Hide the div when clicking anywhere else in the document
+    document.addEventListener('click', (event) => {
+        if (!toggleLink.contains(event.target) && !popup.contains(event.target)) {
+            popup.classList.add('hidden'); // Hide the div
+        }
+    });
