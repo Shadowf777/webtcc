8e04ec1b7de6f68adb22e71685dfe855f21a4cd0
Shadowf777 <samuelgilvane@gmail.com>
2024-11-16 00:17:23 -0300

    fiz os "pop-up"

--- minhaConta/script.js
+++ minhaConta/script.js
@@ -1,22 +1,12 @@
-// Seleciona os elementos
-const btnLapis = document.querySelector('.lapis');  // O botão lápis
-const popupEdicao = document.getElementById('popupEdicao');  // O modal de edição
-const fecharPopup = document.getElementById('fecharPopup');  // O botão de fechar
-const formEdicao = document.getElementById('formEdicao');
+const toggleLink = document.getElementById('dadosPessoais');
+const toggleLink2 = document.getElementById('enderecoPessoal');
 
-// Função para abrir o popup
-btnLapis.addEventListener('click', function () {
-    popupEdicao.classList.remove('hidden');  // Exibe o popup
+toggleLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'dados.html';
 });
 
-// Função para fechar o popup
-fecharPopup.addEventListener('click', function () {
-    popupEdicao.classList.add('hidden');  // Oculta o popup
-});
-
-// Prevenir o envio do formulário (para fins de exemplo)
-formEdicao.addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Dados atualizados!');
-    popupEdicao.classList.add('hidden');  // Fecha o popup após a atualização
+toggleLink2.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'endereco.html';
 });
