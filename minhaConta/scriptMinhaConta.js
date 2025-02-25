2e0fb66e255e92f3dabe4ed70441ab5da2624f1e
Cecília Oliveira <cl203229@g.unicamp>
2025-02-25 15:32:54 -0300

    imagem

--- minhaConta/scriptMinhaConta.js
+++ minhaConta/scriptMinhaConta.js
@@ -13,3 +13,21 @@ var endereco = rua +" "+numero+" - "+bairro+", "+cidade+" - "+estado;
 
 document.getElementById("enderecoUsuario").textContent = endereco;
 
+
+//vuctcukttfuftfydrrycf
+
+function exibirImagem(event) {
+    const input = event.target;
+    const imagemCarregada = document.getElementById('imagemCarregada');
+
+    const file = input.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        imagemCarregada.src = e.target.result;
+        imagemCarregada.classList.add('show');
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
