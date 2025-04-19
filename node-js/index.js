99325be327a8df7b9af57a487f6ad1af8192f3de
GuilhermXd <guilherlie@gmail.com>
2025-04-18 23:33:09 -0300

    con DBs

--- node-js/index.js
+++ node-js/index.js
@@ -1,6 +1,33 @@
+// 
+import mysql from 'mysql2/promise';
 import express from 'express';
-const app = express();
 
-//https://www.youtube.com/watch?v=UdZz8O4IEC4&list=PLMbclvogjXZV7KFtYxXPdCvyJhJkQkGgk
-// get
-app.get
+const app = express();
+async function connectDB()
+{
+    try
+    {
+        const conn = await mysql.createConnection({
+            host: '143.106.241.4',
+            user: 'cl203237',    
+            password: 'cl*31012008',
+            database: 'cl203237'
+        }); 
+        console.log('Conexão Feita');
+        return conn;
+    } catch(err)
+    {
+        console.error('Erro ao Iniciar a conexão: ', err);
+        throw err;
+    }
+}
+
+connectDB();
+
+
+
+
+app.listen(3000, () => {
+    console.log('🔄 Servidor rodando na porta 3000');
+});
+
