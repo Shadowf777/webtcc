d7207d3bea265f1233128680ffe36b95cae27cef
c-pignatellio <cl203229@g.unicamp.br>
2025-05-12 11:48:24 -0300

    sla

--- public/cadastro/login.js
+++ public/cadastro/login.js
@@ -1,81 +1,88 @@
-function login()
-{
+class Usuario {
+    constructor(email, senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    /*constructor(nome, email, celular, cpf, rg, senha)
+    {
+        this.nome = nome;
+        this.email = email;
+        this.celular = celular;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.senha = senha;
+    }*/
+}
+
+var usuarios = [new Usuario("cecilia@gmail.com", "cecilia123")];
+
+function getUsuarios() {
+    return usuarios;
+}
+
+var login = false;
+
+
+
+var r = "";
+
+function fazerLogin(event) {
+    console.log("entrou no fazerLogin");
+    event.preventDefault();
+    var check = false;
+
     var email = document.getElementById("email").value;
     var senha = document.getElementById("senha").value;
 
-    // ação de login
-
-    class Usuario
-    {
-        constructor(email, senha)
-        {
-            this.email = email;
-            this.senha = senha;
-        }
-
-        /*constructor(nome, email, celular, cpf, rg, senha)
-        {
-            this.nome = nome;
-            this.email = email;
-            this.celular = celular;
-            this.cpf = cpf;
-            this.rg = rg;
-            this.senha = senha;
-        }*/
+    if (email == "" || senha == "") {
+        r = "CAMPOS VAZIOS";
+        return "CAMPOS VAZIOS";
+    }
+    else if (email == null || senha == null) {
+        r = "CAMPOS NULOS";
+        return "CAMPOS NULOS";
     }
 
-    var usuarios = [new Usuario("cecilia@gmail.com","cecilia123")];
-
-    function getUsuarios()
-    {
-        return usuarios;
-    }
-
-    var login = false;
-
-
-    function fazerLogin (email, senha)
-    {
-        var check = false;
-        if(login)
-        {
+    else {
+        if (login) {
+            r = "JA TEM LOGIN";
             return "JA TEM LOGIN";
         }
 
-        if(!login)
-        {
+        if (!login) {
             check = true;
             var usuarios = getUsuarios();
-            
-            for(const u of usuarios)
-            {
-                if(u.email!=email)
-                {
+
+            for (const u of usuarios) {
+                if (u.email != email) {
                     check = false;
                 }
-                if(u.senha!= senha)
-                {
+                if (u.senha != senha) {
                     check = false;
                 }
 
-                if(check)
-                {
+                if (check) {
                     login = true;
+                    r = "LOGIN";
                     return "LOGIN";
                 }
-                
+
             }
         }
 
+        r = "NAO FEZ LOGIN";
         return "NAO FEZ LOGIN";
     }
-
-    var r = fazerLogin(email,senha);
-    console.log(r);
-
 }
 
-function mudar()
-{
+
+console.log(r);
+
+
+
+function mudar() {
     window.location.href = "../minhaConta/minhaConta.html";
 }
+
+document.getElementById('formLogin').addEventListener('submit', fazerLogin);
