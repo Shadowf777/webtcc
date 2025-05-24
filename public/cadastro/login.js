7638ba3c0fecd02b333ddb1b3e54f6d76b93e72d
c-pignatellio <cl203229@g.unicamp.br>
2025-05-24 16:45:43 -0300

    ttjt

--- public/cadastro/login.js
+++ public/cadastro/login.js
@@ -1,90 +1,6 @@
 import { setLoggedIn, getLoggedIn, logout } from "../estadoLogin/estadoLogin.js";
 import { getUsuarios } from "../usuario.js";
 
-    constructor(nome, email, celular, cpf, rg, senha)
-    {
-        this._nome = nome;
-        this._email = email;
-        this._celular = celular;
-        this._cpf = cpf;
-        this._rg = rg;
-        this._senha = senha;
-    }
-      
-    // ========== GETTERS ==========
-    get nome() {
-        return this._nome;
-    }
-    
-    get email() {
-        return this._email;
-    }
-    
-    get celular() {
-        return this._celular;
-    }
-    
-    get cpf() {
-        return this._cpf;//.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"); // Formata CPF (000.000.000-00)
-    }
-    
-    get rg() {
-        return this._rg;//.replace(/(\d{2})(\d{3})(\d{3})(\d{1})/, "$1.$2.$3-$4"); // Formata RG (00.000.000-0)
-    }
-    
-
-    get senha() {
-        return this._senha; 
-    }
-    
-    // ========== SETTERS ==========
-    set nome(novoNome) {
-        /*if (typeof novoNome !== "string" || novoNome.trim() === "") {
-        throw new Error("Nome inválido!");
-        }*/
-        this._nome = novoNome.trim();
-    }
-    
-    set email(novoEmail) {
-        /*const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!regexEmail.test(novoEmail)) {
-        throw new Error("E-mail inválido!");
-        }*/
-        this._email = novoEmail;
-    }
-    
-    set celular(novoCelular) {
-        /*const regexCelular = /^\d{10,11}$/; // 10 ou 11 dígitos (DDD + número)
-        if (!regexCelular.test(novoCelular)) {
-        throw new Error("Celular inválido!");
-        }*/
-        this._celular = novoCelular;
-    }
-    
-    set cpf(novoCpf) {
-        /*const cpfLimpo = novoCpf.replace(/\D/g, ""); // Remove não-dígitos
-        if (cpfLimpo.length !== 11) {
-        throw new Error("CPF deve ter 11 dígitos!");
-        }*/
-        this._cpf = novoCpf;
-    }
-    
-    set rg(novoRg) {
-        /*const rgLimpo = novoRg.replace(/\D/g, ""); // Remove não-dígitos
-        if (rgLimpo.length !== 9) {
-        throw new Error("RG deve ter 9 dígitos!");
-        }*/
-        this._rg = novoRg;
-    }
-    
-    set senha(novaSenha) {
-        /*if (novaSenha.length < 6) {
-        throw new Error("Senha deve ter pelo menos 6 caracteres!");
-        }*/
-        this._senha = novaSenha; // Em um caso real, seria criptografada
-    }
-      
-}
 
 ////////////////////////////////////////////////
 
@@ -191,4 +107,3 @@ function mudar() {
 }
 
 document.getElementById('formLogin').addEventListener('submit', fazerLogin);
-
