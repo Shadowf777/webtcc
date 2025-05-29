c4fc28bb5f39b6cba3bbb843112fe73f1cd4e867
c-pignatellio <cl203229@g.unicamo.br>
2025-05-29 08:28:01 -0300

    ngg

--- view/esqueceuASenha/esqueceu.php
+++ view/esqueceuASenha/esqueceu.php
@@ -21,11 +21,7 @@
         <strong>Esqueceu sua senha?</strong>
         <p>Preencha seu email, e um código de verificação será enviado.</p>
 
-<<<<<<< HEAD
-        <form action="codigo.php">
-=======
         <form id="formEmail" action="codigo.php">
->>>>>>> be1af0933807d7a552ac70e8133e42a0361fd257
             <input id="email" type="email" placeholder="Seu email" required><br>
             <input type="submit" value="Enviar">
         </form>
@@ -39,14 +35,9 @@
     <script type="module" src="../../public/styleSite/stylePop.js"></script>
     <script src="../../public/scriptPopUp.js"></script>
     <script src="../../public/menuResponsivo.js"></script>
-<<<<<<< HEAD
 
 
-   
-
-=======
     <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
     <script src="../../public/esqueceuASenha/mandarEmail.js"></script>
->>>>>>> be1af0933807d7a552ac70e8133e42a0361fd257
 </body>
 </html>
