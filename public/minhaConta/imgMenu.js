0b8891a06bab1f6c9d091de8ccd0f986c47155b1
c-pignatellio <cl203229@g.unicamp.br>
2025-06-09 10:14:19 -0300

    vrve

--- public/minhaConta/imgMenu.js
+++ public/minhaConta/imgMenu.js
@@ -3,15 +3,12 @@ import { getFotoPerfil, getLoggedIn } from "../estadoLogin/estadoLogin.js";
 const imgDefault = '../../public/imagens/perfildefault.jpg'; // imagem default
 const img = '../../public/imagens/jacinto.jpg';
 var foto = document.querySelectorAll(".imgMenu");
-const img = "../../public/imagens/jacinto.jpg";
+//const img = "../../public/imagens/jacinto.jpg";
 if (getLoggedIn()) {
     foto.forEach(imagem => {
-<<<<<<< HEAD
         //imagem.src =img; //getFotoPerfil();
-=======
         imagem.src =img;
->>>>>>> e6781026f0ff8e963e4b8fc553395f20d3764890
-    });
+})
 }
 else if(!getLoggedIn())
 {
