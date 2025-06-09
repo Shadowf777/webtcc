8918742f3d14721845f01f0cd48ae08a37d4ce29
c-pignatellio <cl203229@g.unicamp.br>
2025-06-09 11:10:14 -0300

    yhyt

--- view/powerBI/powerBI.php
+++ view/powerBI/powerBI.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/styleSite/style.css">
-    <link rel="stylesheet" href="../../public/index/styleIndex.css">
+    <link rel="stylesheet" href="../../public/powerBI/powerBI.css">
     <link rel="icon" type="image/x-icon" href="../../public/imagens/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Página Inicial</title>
@@ -18,7 +18,20 @@
 
 <div class="container_" id="container_" tabindex="0" onfocus="closeSidebar()">
        
-<div></div>
+<div class="power">
+    <div class="tituloPower">Estatísticas sobre a EcoStuff</div>
+
+    <p>Este painel foi feito com a plataforma Power BI como forma de demonstrar os dados cadastrados na EcoStuff.</P>
+
+    <div class="powerbi">
+
+    </div>
+
+
+
+
+
+</div>
 
         
 
