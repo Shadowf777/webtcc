fb7974a3f4818ac1c69879a33bc57d08212278aa
c-pignatellio <cl203229@g.unicamp.br>
2025-06-12 08:35:39 -0300

    precisa mudar o style do swal e responsividade do link do powerbi

--- public/minhaConta/scriptMinhaConta.js
+++ public/minhaConta/scriptMinhaConta.js
@@ -1,4 +1,4 @@
-import { setLoggedIn } from "../estadoLogin/estadoLogin.js";
+import { setLoggedIn, logout } from "../estadoLogin/estadoLogin.js";
 import { apagarConta } from "../usuario.js";
 
 document.getElementById("excluir").addEventListener('click', excluir);
@@ -10,14 +10,21 @@ function excluir() {
     title: "Está ação irá excluir sua conta permanentemente. Confirma a exclusão?",
     showDenyButton: true,
     confirmButtonText: "Excluir",
-    denyButtonText: `Não excluir`
+    denyButtonText: `Não excluir`,
+    customClass: {
+      popup: 'my-swal-popup',
+      title: 'my-swal-title',
+      content: 'my-swal-content',
+      confirmButton: 'my-swal-confirm-button',
+      denyButton: 'my-swal-deny-button',
+    }
   }).then((result) => {
 
     if (result.isConfirmed) {
       Swal.fire("Conta excluída!", "", "success").then(() => {
         window.location.href = '../index/index.php';
         apagarConta();
-        setLoggedIn(false);
+        logout();
       });
 
     } else if (result.isDenied) {
